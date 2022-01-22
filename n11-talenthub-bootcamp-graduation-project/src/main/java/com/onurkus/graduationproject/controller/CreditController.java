28b082820238f3607f705468ff71cc0b9bddcb6e
onurkus7 <46908015+onurkus7@users.noreply.github.com>
2022-01-22 23:18:04 +0300

    Controllers added

---
+++ n11-talenthub-bootcamp-graduation-project/src/main/java/com/onurkus/graduationproject/controller/CreditController.java
@@ -0,0 +1,11 @@
+package com.onurkus.graduationproject.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("app/credits")
+@AllArgsConstructor
+public class CreditController {
+}
---
+++ n11-talenthub-bootcamp-graduation-project/src/main/java/com/onurkus/graduationproject/controller/MessageController.java
@@ -0,0 +1,11 @@
+package com.onurkus.graduationproject.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("app/messages")
+@AllArgsConstructor
+public class MessageController {
+}
