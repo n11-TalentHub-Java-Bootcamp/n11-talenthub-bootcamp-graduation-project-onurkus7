6f678f92aa04396dca6aac7ffd0782afb9a73bd0
onurkus7 <46908015+onurkus7@users.noreply.github.com>
2022-01-26 01:45:04 +0300

    Dtos added

--- n11-talenthub-bootcamp-graduation-project/src/main/java/com/onurkus/graduationproject/credit/dto/CreditDto.java
+++ n11-talenthub-bootcamp-graduation-project/src/main/java/com/onurkus/graduationproject/credit/dto/CreditDto.java
@@ -1,4 +1,16 @@
 package com.onurkus.graduationproject.credit.dto;
 
+import com.onurkus.graduationproject.credit.enums.EnumCreditStatus;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class CreditDto {
+
+    private Long customerId;
+    private Integer creditScore;
+    private EnumCreditStatus creditStatus;
+    private BigDecimal creditLimit;
+
 }
