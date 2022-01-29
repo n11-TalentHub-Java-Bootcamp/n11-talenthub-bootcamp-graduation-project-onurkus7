5cc5f7a9593554585eedeb2a1cd1759b80bbb1c2
onurkus7 <46908015+onurkus7@users.noreply.github.com>
2022-01-29 22:23:43 +0300

    Repository layers fixed

--- n11-talenthub-bootcamp-graduation-project/src/main/java/com/onurkus/graduationproject/message/repository/MessageRepository.java
+++ n11-talenthub-bootcamp-graduation-project/src/main/java/com/onurkus/graduationproject/message/repository/MessageRepository.java
@@ -1,12 +1,12 @@
-package com.onurkus.graduationproject.message.dao;
+package com.onurkus.graduationproject.message.repository;
 
 import com.onurkus.graduationproject.message.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageDao extends JpaRepository<Message,Long> {
+public interface MessageRepository extends JpaRepository<Message,Long> {
 
-    Message findMessageByIdentityId(Long identityId);
+
 
 }
