bcbf9f5ddacf625094490a05c223a780d11f1e50
onurkus7 <46908015+onurkus7@users.noreply.github.com>
2022-01-30 20:17:45 +0300

    Repository layers fixed

--- src/main/java/com/onurkus/graduationproject/credit/repository/CreditRepository.java
+++ src/main/java/com/onurkus/graduationproject/credit/repository/CreditRepository.java
@@ -2,9 +2,11 @@ package com.onurkus.graduationproject.credit.repository;
 
 import com.onurkus.graduationproject.credit.entity.Credit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.Date;
 import java.util.List;
 
@@ -14,5 +16,10 @@ public interface CreditRepository extends JpaRepository<Credit, Long> {
     @Query("SELECT a FROM Credit a, Customer b WHERE a.identityId= b.identityId AND a.identityId= :identityId AND" +
             " b.birthdayDate= :birthdayDate ORDER BY a.creationDate DESC")
     List<Credit> findCreditByIdentityIdAndBirthdayDate(Long identityId, Date birthdayDate);
+
+    @Transactional
+    @Modifying
+    @Query(" DELETE FROM Credit a WHERE a.identityId= :identityId ")
+    void deleteCreditByIdentityId(Long identityId);
 
 }
--- src/main/java/com/onurkus/graduationproject/message/repository/MessageRepository.java
+++ src/main/java/com/onurkus/graduationproject/message/repository/MessageRepository.java
@@ -2,11 +2,18 @@ package com.onurkus.graduationproject.message.repository;
 
 import com.onurkus.graduationproject.message.entity.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message,Long> {
 
-
+    @Transactional
+    @Modifying
+    @Query(" DELETE FROM Message a WHERE a.identityId= :identityId ")
+    void deleteMessageByIdentityId(Long identityId);
 
 }
