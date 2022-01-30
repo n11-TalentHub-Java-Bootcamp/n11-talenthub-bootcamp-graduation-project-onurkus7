1b753192c71d910ef21f47d130007a6188c54d35
onurkus7 <46908015+onurkus7@users.noreply.github.com>
2022-01-30 20:18:10 +0300

    Message entity fixed

--- src/main/java/com/onurkus/graduationproject/message/entity/Message.java
+++ src/main/java/com/onurkus/graduationproject/message/entity/Message.java
@@ -6,27 +6,27 @@ import lombok.Data;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "MESSAGE")
+@Table(name = "MESSAGES")
 @Data
 public class Message {
 
     @Id
-    @SequenceGenerator(name = "Message", sequenceName = "MESSAGE_ID_SEQ")
-    @GeneratedValue(generator = "Message")
-    @Column(name = "MESSAGE_ID", nullable = false, unique = true)
+    @SequenceGenerator(name = "Messages", sequenceName = "MESSAGES_ID_SEQ")
+    @GeneratedValue(generator = "Messages")
+    @Column(name = "MESSAGES_ID", nullable = false, unique = true)
     private Long id;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "CUSTOMER_ID", foreignKey = @ForeignKey(name = "FK_MESSAGE_CUSTOMER_ID"))
+    @JoinColumn(name = "CUSTOMER_ID", foreignKey = @ForeignKey(name = "FK_MESSAGES_CUSTOMER_ID"))
     private Customer customerId;
 
     @Column(length = 11, name = "IDENTITY_ID", nullable = false, updatable = false)
     private Long identityId;
 
-    @Column(name = "PHONE_NUMBER", unique = true)
+    @Column(name = "PHONE_NUMBER")
     private String phoneNumber;
 
-    @Column(length = 200, name = "MESSAGE_CONTENT")
+    @Column(length = 200, name = "MESSAGES_CONTENT")
     private String messageContent;
 
 }
